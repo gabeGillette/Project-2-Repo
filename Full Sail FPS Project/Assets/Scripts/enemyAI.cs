ff84fe4df4e5f73776a83727d7f58ddb75e6b32a
Adam McKee <dedicated06@hotmail.com>
2024-11-03 14:40:58 -0800

    Created enemyAI
    Created the enemyAI along with IDamage script. Right now just have it so the enemy moves/reacts when player is near.
    However, the enemy is not moving towards the player and I am not sure if that is because there are private variables in the game manager script that are set up for _playerState.

---
+++ Full Sail FPS Project/Assets/Scripts/enemyAI.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyAI : MonoBehaviour, IDamage
+{
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Renderer model;
+    [SerializeField] Transform shootPos;
+    [SerializeField] Transform headPos;
+
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+
+    [SerializeField] GameObject spit;
+    [SerializeField] float spitRate;
+    [SerializeField] float meleeAttackRate;
+    [SerializeField] bool rangedAttacker;
+
+    Color colorOrig;
+
+    bool isSpiting;
+    bool isMeleeAttacking;
+    bool playerInSpitRange;
+    bool playerInMeleeRange;
+    bool canSpit;
+
+    Vector3 playerDir;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Setting the original color so we can change it later to show damage
+        colorOrig = model.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move enemy towards player and if ranged attacker start ranged attack
+        if (playerInSpitRange)
+        {
+            //if (rangedAttacker)
+            //{
+            //    canSpit = true;
+            //}
+
+            playerDir = GameManager.instance.GetPlayer().transform.position - headPos.position;
+
+            agent.SetDestination(GameManager.instance.GetPlayer().transform.position);
+
+            //Moves the enemy towards the player
+            if(agent.remainingDistance <= agent.stoppingDistance)
+            {
+                faceTarget();
+            }
+
+            //If within melee range stops spit attack but allows melee attack
+            //if (playerInMeleeRange)
+            //{
+
+            //}
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInSpitRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInSpitRange = false;
+        }
+    }
+
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        StartCoroutine(damageFlash());
+
+        //If the enemy takes damage outside of normal agro range head towards the player's last attack position
+        agent.SetDestination(GameManager.instance.GetPlayer().transform.position);
+
+        //Destroy the object
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //A way to demo the enemy took damage. Change the Color.red to other colors if necessary
+    IEnumerator damageFlash()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.2f);
+        model.material.color = colorOrig;
+    }
+
+    //Process to start spitting/shooting
+    IEnumerator shoot()
+    {
+        
+        isSpiting = true;
+
+        Instantiate(spit, shootPos.position, transform.rotation);
+
+        yield return new WaitForSeconds(spitRate);
+        isSpiting = false;
+    }
+
+    //Allows enemy to do a melee attack if within range
+    IEnumerator meleeAttack()
+    {
+        isMeleeAttacking = true;
+
+        yield return new WaitForSeconds(meleeAttackRate);
+        isMeleeAttacking = false;
+    }
+
+    //Makes the enemy always look towards the player smoothly
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(playerDir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+}
