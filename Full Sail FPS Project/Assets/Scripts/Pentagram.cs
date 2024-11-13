73fd8d24628bbf028dc1bc3c3b48aec7fcac2be0
Kenton Weaver <kentgweaver@gmail.com>
2024-11-13 13:00:15 -0600

    Added Pentagram code
    New script for Pentagram code

---
+++ Full Sail FPS Project/Assets/Scripts/Pentagram.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pentagram : MonoBehaviour, IInteractable
+{
+    [SerializeField] Color skyColorChange = Color.red;  // Change to desired color
+    [SerializeField] float spawnRadius = 10f;           // Radius around the pentagram to spawn monsters
+    [SerializeField] GameObject monsterPrefab;          // Assign your monster prefab in the Inspector
+    [SerializeField] int numMonsters = 5;               // Number of monsters to spawn
+
+    private bool hasBeenActivated = false;
+
+    public void OnInteract()
+    {
+        if (!hasBeenActivated)
+        {
+            hasBeenActivated = true;
+            ChangeSkyColor();
+            StartMonsterSummoning();
+        }
+    }
+
+    private void ChangeSkyColor()
+    {
+        RenderSettings.ambientLight = skyColorChange;   // Simple sky color change
+    }
+
+    private void StartMonsterSummoning()
+    {
+        for (int i = 0; i < numMonsters; i++)
+        {
+            Vector3 spawnPos = transform.position + Random.insideUnitSphere * spawnRadius;
+            spawnPos.y = transform.position.y; // Keep monsters on the same level as the pentagram
+
+            Instantiate(monsterPrefab, spawnPos, Quaternion.identity);
+        }
+    }
+}
